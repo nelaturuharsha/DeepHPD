0469e29fb5efa6992a0a780a3c755764bddf16b2
Sree Harsha Nelaturu <nelaturu.harsha@gmail.com>
2017-11-24 23:36:02 +0530

    Add files via upload

---
+++ Inference.py
@@ -0,0 +1,55 @@
+"""
+Run the online classification system.
+
+Capture an image, classify, do it again.
+"""
+import time
+#from picamera import PiCamera
+#from picamera.array import PiRGBArray
+import tensorflow as tf
+import cv2
+import os
+
+def get_labels():
+    """Get a list of labels so we can see if it's an ad or not."""
+    with open('./inception/retrained_labels.txt', 'r') as fin:
+        labels = [line.rstrip('\n') for line in fin]
+    return labels
+
+def run_classification(labels):
+    """Stream images off the camera and process them."""
+
+    with tf.gfile.FastGFile("./inception/retrained_graph.pb", 'rb') as fin:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(fin.read())
+        _ = tf.import_graph_def(graph_def, name='')
+
+    with tf.Session() as sess:
+        # And capture continuously forever.
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+        #for video in os.listdir("./predictimage"):
+        cam = cv2.VideoCapture("./20171124_224528.mp4")
+        while True:
+            ret, frame = cam.read()
+            #frame = cv2.resize(frame, (1920, 1080))
+            #cv2.imshow("frame", frame)
+            decoded_image = frame
+
+        # Make the prediction. Big thanks to this SO answer:
+        # http://stackoverflow.com/questions/34484148/feeding-image-data-in-tensorflow-for-transfer-learning
+            predictions = sess.run(softmax_tensor, {'DecodeJpeg:0': decoded_image})
+            prediction = predictions[0]
+
+            # Get the highest confidence category.
+            prediction = prediction.tolist()
+            max_value = max(prediction)
+            max_index = prediction.index(max_value)
+            predicted_label = labels[max_index]
+            #return (predicted_label)
+            #print("%s (%.2f%%)" % (predicted_label, max_value * 100))
+            print(predicted_label)
+            # Reset the buffer so we're ready for the next one.
+            #raw_capture.truncate(0)
+
+if __name__ == '__main__':
+    run_classification(get_labels())
---
+++ LED-Blink.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+from online.py import *
+
+string = get_classifications()
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(7,GPIO.OUT)
+print "LED on"
+if(string == 'human'):
+  GPIO.output(7,GPIO.HIGH)
+else:
+    time.sleep(1)
+print "LED off"
+GPIO.output(7,GPIO.LOW)
